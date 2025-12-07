f6130dc91eadd86f4d98104625f1475d6c0772b8
Saghir Ahmed <saghirx@gmail.com>
2025-12-07 12:51:34 +0000

    added src script

---
+++ src/net/skny/DynamicUtils.groovy
@@ -0,0 +1,13 @@
+package net/skny
+
+class DynamicUtils implements Serializable {
+    def script
+
+    DynamicUtils(script) {
+        this.script = script
+    }
+
+    void logCurrentTime() {
+        script.echo "Current time (from DynamicUtils): ${new Date()}"
+    }
+}
